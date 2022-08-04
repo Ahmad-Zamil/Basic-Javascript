51c595cf8070cf90a86132b1208196d178946418
Ahmad-Zamil <ahmadzamil419@gmail.com>
2022-08-04 23:43:37 +0600

    comparison

---
+++ comparison.js
@@ -0,0 +1,23 @@
+// Compare
+console.log(5 < 6);
+console.log(5 > 6);
+console.log(5 == 6);
+console.log(6 == 6);
+console.log(5 != 6);   //[Not Equal !]
+console.log(5 != 5);   //[Not Equal !]
+console.log(5 <= 6);
+console.log(6 <= 6);
+console.log(16 <= 6);
+
+//greater than or equal
+
+console.log(5 >= 6);
+console.log(5 >= 2);
+console.log(2 >= 2);
+
+// multipule consitions
+//and
+money1 > money2 && result1 > result2 && height1 > height2  //[and = &&]
+
+// or
+money1 > money2 || result1 > result2 || height1 > height2  //[and =||]
