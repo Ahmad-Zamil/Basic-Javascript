675667efdfc0af24440857887782098709f38075
Ahmad-Zamil <ahmadzamil419@gmail.com>
2022-08-04 23:30:28 +0600

    push-pop

---
+++ push-pop.js
@@ -0,0 +1,18 @@
+// Add (push) element
+
+var numbers = [78, 45, 98, 45];
+numbers.push(63);
+numbers.push(85);
+console.log(numbers);
+
+// Remove (pop) element
+
+numbers.pop();
+console.log(numbers);
+
+// Remove (pop) element and decclare
+
+var element = numbers.pop();
+numbers.pop();
+console.log(numbers);
+console.log(element);
